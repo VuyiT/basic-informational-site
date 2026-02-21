0f94aaffd02ce006a3cf4b470410c490a8b9678b
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-21 18:44:04 +0200

    Read html files and create a basic routing system

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 #!/usr/bin/env node
 
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
+
+    let path = "./views/";
+
+    switch(req.url) {
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        case "/contact-me":
+            path += "contact-me.html";
+            break;
+        default:
+            path += "404.html";
+            break;        
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
     console.log("request made");
-    res.end();
 });
 
 server.listen(8080, "localhost", () => {
     console.log("Listening for requests on port 8080");
-})
+});
