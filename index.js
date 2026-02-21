d7d1618f65e9fc415e7ffa566dd15677c485562e
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-21 18:24:16 +0200

    Create server

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    console.log("request made");
+    res.end();
+});
+
+server.listen(8080, "localhost", () => {
+    console.log("Listening for requests on port 8080");
+})
